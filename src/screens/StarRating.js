a3b2ebeec215f0118caf152a7c618499ea77c510
ivyblue092 <knezovicivanka092@gmail.com>
2020-07-16 01:29:41 +0200

    appshop

---
+++ src/screens/StarRating.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+const Star = ({ starId, marked }) => {
+  return (
+    <span
+      star-id={starId}
+      role="button"
+      style={{ color: "#ff9933", cursor: "pointer" }}
+    >
+      {marked ? "\u2605" : "\u2606"}
+    </span>
+  );
+};
+
+// Create an array of 5: Array.from({length: 5}, (v,i) => i)
+const StarRating = props => {
+  // Manages on Hover selection of a star
+  const [selection, setSelection] = React.useState(0);
+
+  // Manages rating selection
+  const [rating, setRating] = React.useState(0);
+
+  const hoverOver = event => {
+    let starId = 0;
+    if (event && event.target && event.target.getAttribute("star-id")) {
+      starId = event.target.getAttribute("star-id");
+    }
+    setSelection(starId);
+  };
+
+  return (
+    <div
+      onMouseOver={hoverOver}
+      onMouseOut={() => hoverOver(null)}
+      onClick={event => setRating(event.target.getAttribute("star-id"))}
+    >
+      {Array.from({ length: 5 }, (v, i) => (
+        <Star starId={i + 1} marked={selection ? selection > i : rating > i} />
+      ))}
+    </div>
+  );
+};
+
+export default StarRating;
